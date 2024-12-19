d13851318dab185c658f933772ff9a0cf88818f7
Роман <150062689+romashka-kop@users.noreply.github.com>
2024-12-19 11:02:44 +0700

    a

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,23 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
+
 
 public class MenuManager : MonoBehaviour
 {
     public GameObject PanelSettings;
     public GameObject PanelNewGame;
 
+    public Button ContinueButton;
+    public Button NewGameButton;
+    public Button SettingsButton;
+    public Button QuitButton;
+
     private static bool _isOpenedSettings = false;
+    private static bool _isOpenedNewGame = false;
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        ContinueButton.onClick.AddListener(Continue);
+        NewGameButton.onClick.AddListener(NewGame);
+        SettingsButton.onClick.AddListener(Settings);
+        QuitButton.onClick.AddListener(ExitGame);
+        PanelSettings.SetActive(_isOpenedSettings);
+        PanelNewGame.SetActive(_isOpenedNewGame);
+    }
 
     public void Continue()
     {
-        SceneManager.LoadScene("");
+        Debug.Log("Продолжить");
     }
     public void NewGame()
     {
-        SceneManager.LoadScene("");
+        _isOpenedNewGame = !_isOpenedNewGame;
+        PanelNewGame.SetActive(_isOpenedNewGame);
     }
     public void Settings()
     {

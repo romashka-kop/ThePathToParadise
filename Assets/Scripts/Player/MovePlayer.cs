09bed5e28d487fa52e8215b3a4c235c695dd5944
Роман <150062689+romashka-kop@users.noreply.github.com>
2025-01-16 23:20:57 +0700

    Грубо работает вращение персонажем, управление работает и связано с сохранениями

--- Assets/Scripts/Player/MovePlayer.cs
+++ Assets/Scripts/Player/MovePlayer.cs
@@ -5,46 +5,61 @@ using UnityEngine;
 public class MovePlayer : MonoBehaviour
 {
     public float Speed = 0.5f;
+    private float _verticalRotation = 0f;
+
     public GameObject SettingsCanvas;
-
-    private Rigidbody rb;
-    private Vector3 _moveVector;
-
-    void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
 
     void FixedUpdate()
     {
         MovePlayerPosition();
-        RotatePlayer();
         OpenPauseMenu();
     }
 
     private void MovePlayerPosition()
     {
-        _moveVector.x = Input.GetAxis("Horizontal");
-        _moveVector.z = Input.GetAxis("Vertical");
-        rb.MovePosition(rb.position + _moveVector * Speed * Time.deltaTime);
+        float mouseX = Input.GetAxis("Mouse X") * MenuManager.DataSettings.Sensivity;
+        float mouseY = Input.GetAxis("Mouse Y") * MenuManager.DataSettings.Sensivity;
 
+        _verticalRotation -= mouseY * Time.deltaTime;
+        _verticalRotation = Mathf.Clamp(_verticalRotation, -80f, 80f);
 
-        //if (Input.GetKeyDown())
-        //{
+        Camera.main.transform.localRotation = Quaternion.Euler(_verticalRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * (mouseX * Time.deltaTime));
 
-        //}
-    }
+        Vector3 movement = Vector3.zero;
 
-    private void RotatePlayer()
-    {
-        
+        #region Расшифровка массива кнопок
+        //Forward = MenuManager.DataSettings.PlayerControlKeyCode[0]
+        //Backward = MenuManager.DataSettings.PlayerControlKeyCode[1]
+        //Left = MenuManager.DataSettings.PlayerControlKeyCode[2]
+        //Right = MenuManager.DataSettings.PlayerControlKeyCode[3]
+        //Jump = MenuManager.DataSettings.PlayerControlKeyCode[4]
+        //Squat = MenuManager.DataSettings.PlayerControlKeyCode[5]
+        #endregion
+
+        if (Input.GetKey(MenuManager.DataSettings.PlayerControlKeyCode[0])) 
+            movement += transform.forward;
+            
+        if (Input.GetKey(MenuManager.DataSettings.PlayerControlKeyCode[1])) 
+            movement -= transform.forward;
+            
+        if (Input.GetKey(MenuManager.DataSettings.PlayerControlKeyCode[2])) 
+            movement -= transform.right;
+            
+        if (Input.GetKey(MenuManager.DataSettings.PlayerControlKeyCode[3])) 
+            movement += transform.right;
+
+        movement.Normalize();
+        movement *= Speed * Time.deltaTime;
+
+        transform.position += movement;
     }
 
     private void OpenPauseMenu()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            SettingsCanvas.SetActive(true);
+
         }
     }
 }
--- Assets/Scripts/SaveData/SaveDataSettings.cs
+++ Assets/Scripts/SaveData/SaveDataSettings.cs
@@ -12,8 +12,8 @@ public class SaveDataSettings : IData
     #region Настройки Управления
     public KeyCode[] PlayerControlKeyCode = {KeyCode.W, KeyCode.S, KeyCode.A, KeyCode.D, KeyCode.Space, KeyCode.LeftControl};
 
-    public bool IsOnVSync = true;
-    public float Sensivity = 30f;
+    public bool IsOnVSync = false;
+    public float Sensivity = 300f;
     #endregion
 
     #region Настройки Звуков
